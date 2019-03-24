885d2cf594c7deeb73a9f1c1aceb6100112096c9
gatis <gatis.steinbergs@gmail.com>
2019-03-24 12:51:16 +0200

    Initial commit

---
+++ ingredient-service/src/main/java/com/springia/ingredientservice/IngredientServiceApplication.java
@@ -0,0 +1,12 @@
+package com.springia.ingredientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngredientServiceApplication.class, args);
+    }
+}
